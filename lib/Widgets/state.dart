68ca3202657147242da2d3ad4bcf8db0e100d630
yhhCh405-CS <yehtet10@cloudsource.co.jp>
2021-05-02 13:04:17 +0630

    added for statelesswidget

--- lib/Widgets/state.dart
+++ lib/Widgets/state.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
-abstract class CoreState<T extends StatefulWidget> extends State {
+abstract class _Common {
   void showLongToast(String message);
   void showShortToast(String message);
 
   void showSnackBar(String message);
   void showSuccessSnackBar(String message);
   void showErrorSnackBar(String message);
-  
+
   void showAlertDialog(String message);
   void showSuccessDialog(String message);
   void showErrorDialog(String message);
 }
+
+abstract class CoreState<T extends StatefulWidget> extends State implements _Common {}
+
+abstract class CoreStatelessWidget extends StatelessWidget implements _Common {}
