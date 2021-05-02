d6c1a6bf11c9b1ef6f3fc14ceb04797c3d5f773d
yhhCh405-CS <yehtet10@cloudsource.co.jp>
2021-05-02 13:08:52 +0630

    init for null-safety

--- lib/Networking/result.dart
+++ lib/Networking/result.dart
@@ -1,18 +1,18 @@
 class Result<T>{
   bool get success => this._success;
-  String get errorMessage => this._errorMessage;
-  T get data => this._data;
+  String? get errorMessage => this._errorMessage;
+  T? get data => this._data;
 
-  bool _success;
-  String _errorMessage;
-  T _data;
+  bool _success = false;
+  String? _errorMessage;
+  T? _data;
 
   Result.success(this._data){
     this._success = true;
     this._errorMessage = null;
   }
 
-  Result.error(this._errorMessage,{T result}){
+  Result.error(this._errorMessage,{T? result}){
     this._success = false;
     this._data = result;
   }
