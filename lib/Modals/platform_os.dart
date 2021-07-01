9b41e8e4596879eab29c675081ce21351e1b09b0
yhhCh405-CS <yehtet10@cloudsource.co.jp>
2021-07-01 11:23:10 +0630

    added_platform_os

---
+++ lib/Modals/platform_os.dart
@@ -0,0 +1,8 @@
+class PlatformOS {
+  static const Android = "android";
+  static const iOS = "ios";
+  static const MacOS = "macos";
+  static const Linux = "linux";
+  static const Windows = "windows";
+  static const Fuchsia = "fuchsia";
+}
